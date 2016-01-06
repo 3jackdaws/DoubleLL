7c923648feb01f2545ea4fdb201dca647e48f331
Ian Murphy <ian@thanatos.local>
2016-01-06 14:18:50 -0800

    Initial Commit

---
+++ DoubleLL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DoubleLL
+//
+//  Created by Ian Murphy on 1/6/16.
+//  Copyright © 2016 Ian Murphy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
